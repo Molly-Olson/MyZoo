8ad0cf1ae09541940b004944965cddd1318111d2
Molly Olson <molly.olson@student.centralia.edu>
2026-04-12 14:40:58 -0700

    I broke it, may start over Tried following a youtube dude and now I have an exception error

--- MyZoo/Program.cs
+++ MyZoo/Program.cs
@@ -1,4 +1,7 @@
-﻿using MyZoo;
+﻿using System.Reflection;
+using MyZoo;
+//using static System.Console; k so how do I make this nifty trick work?
+// I added this and removed the keyword console below to test and now I am getting an exception error???
 
 namespace MyZoo
 {
@@ -6,7 +9,17 @@ namespace MyZoo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to My zoo, these are infact MY monkeys!");
+            Console.WriteLine(@" __  __       _ _       _       __  __             _           _ 
+|  \/  | ___ | | |_   _( )___  |  \/  | __ _  __ _(_) ___ __ _| |
+| |\/| |/ _ \| | | | | |// __| | |\/| |/ _` |/ _` | |/ __/ _` | |
+| |  | | (_) | | | |_| | \__ \ | |  | | (_| | (_| | | (_| (_| | |
+|_|  |_|\___/|_|_|\__, | |___/ |_|  |_|\__,_|\__, |_|\___\__,_|_|
+|  \/  | ___ _ __ |___/_  __ _  ___ _ __(_) _|___/               
+| |\/| |/ _ \ '_ \ / _` |/ _` |/ _ \ '__| |/ _ \                 
+| |  | |  __/ | | | (_| | (_| |  __/ |  | |  __/                 
+|_|  |_|\___|_| |_|\__,_|\__, |\___|_|  |_|\___|                 
+                         |___/                                   ");
+            Console.WriteLine("Welcome to My zoo, this is my circus and these are infact MY monkeys!");
             Animal a = new Animal("Billie", 2);
             Animal b = new Animal("Kimball", 4);
             a.MakeSound();
